670079f206202fa21e2eeb14bbaf93d0f02366a8
mybaseball52 <mybaseball52@gmail.com>
2021-11-24 20:05:21 +0800

    [refactor] change days calculation same as start and end time

--- BudgetSystem/BudgetSystem/BudgetService.cs
+++ BudgetSystem/BudgetSystem/BudgetService.cs
@@ -37,26 +37,24 @@ namespace BudgetSystem
                         int days;
                         if (currentYearMonth.ToString("yyyyMM") == start.ToString("yyyyMM"))
                         {
-                            // var lastDayOfStartMonth = new DateTime(start.Year, start.Month, DateTime.DaysInMonth(start.Year, start.Month));
                             var overlappingStart = new DateTime(start.Year, start.Month, DateTime.DaysInMonth(start.Year, start.Month));
                             var overlappingEnd = start;
-                            // days = (lastDayOfStartMonth - start).Days+1;
                             days = (overlappingStart - overlappingEnd).Days+1;
                         }
                         else if (currentYearMonth.ToString("yyyyMM") == end.ToString("yyyyMM"))
                         {
-                             // var firstDayOfEndMonth = new DateTime(end.Year, end.Month, 1);
                              var overlappingStart = end;
                              var overlappingEnd = new DateTime(end.Year, end.Month, 1);
-                             // days = (end - firstDayOfEndMonth).Days + 1; 
                              days = (overlappingStart - overlappingEnd).Days + 1; 
                         }
                         else
                         {
-                            var overlappingStart = currentYearMonth.Year;
-                            var overlappingEnd = currentYearMonth.Month;
-                            days = DateTime.DaysInMonth(overlappingStart, overlappingEnd);
-                            // days = DateTime.DaysInMonth(currentYearMonth.Year, currentYearMonth.Month);
+                            // var overlappingStart = currentYearMonth.Year;
+                            // var overlappingEnd = currentYearMonth.Month;
+                            // days = DateTime.DaysInMonth(overlappingStart, overlappingEnd);
+                            var overlappingStart = new DateTime(currentYearMonth.Year, currentYearMonth.Month, DateTime.DaysInMonth(currentYearMonth.Year, currentYearMonth.Month));
+                            var overlappingEnd = new DateTime(currentYearMonth.Year, currentYearMonth.Month, 1);
+                            days = (overlappingStart - overlappingEnd).Days + 1;
                         }
 
                         var budget = allAmount.FirstOrDefault(x => x.YearMonth.Equals(currentYearMonth.ToString("yyyyMM")));
